0af879d9da2f3b93ab82a5f63b283da882c7e01d
Margaret Kelly <margaretkelly@Margarets-Mac-mini.local>
2021-12-18 11:53:55 -0500

    need to get seasn/:id handler working. Other handlers seem fine

--- index.js
+++ index.js
@@ -3,28 +3,24 @@ const showdata = require('./showdata')
 
 const app = express()
 
- app.use(express.static('public'))
+app.use(express.static('public'))
 
- app.set('view engine', 'pug')
+app.set('view engine', 'pug')
 
-
-app.get('/', (request, response) => {
-    return response.render('index', { showdata })
+app.get('/', (request, response) =>{
+   return response.render('index', { showdata })
 })
-
-app.get('/', (request, response) => {
-    const season = showdata.seasons.find((season) => {season.number === parseInt(request.params.id)
-
-    })
-    return response.render('season', { season, title: showdata.title})
-})
-
-
+/*
+app.get('season/:id', (request, response) => {
+    const season = showdata.seasons.find(seasons => season.number  === parseInt(request.params.id)
+    
+    // return response.render('season', { season, title: showdata.title})
+//})
+*/
 
 app.all ('*', (request, response) => {
     return response.sendStatus(404)
 })
 
-app.listen (1337, () => [
-    console.log('listening on port 1337...') //eslint-disable-line no-comsole
-])
+app.listen (1337, () =>{console.log('listening on port 1337...') //eslint-disable-line no-comsole
+})
