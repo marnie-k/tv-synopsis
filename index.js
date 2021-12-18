af37795df71a9e91430f20ed953bd83a8ecdfa39
Margaret Kelly <margaretkelly@Margarets-Mac-mini.local>
2021-12-18 13:36:22 -0500

    my wonderful husband put his OCD to use and found my typo!  Links are workinggit add .

--- index.js
+++ index.js
@@ -6,21 +6,20 @@ const app = express()
 app.use(express.static('public'))
 
 app.set('view engine', 'pug')
-
-app.get('/', (request, response) =>{
+//handler #1
+app.get('/', (request, response) => {
    return response.render('index', { showdata })
 })
-/*
-app.get('season/:id', (request, response) => {
-    const season = showdata.seasons.find(seasons => season.number  === parseInt(request.params.id)
-    
-    // return response.render('season', { season, title: showdata.title})
-//})
-*/
+//handler #2
+app.get('/season/:id', (request, response) => {
+    const season = showdata.seasons.find(season => season.number  === parseInt(request.params.id))
 
+    return response.render('season', { season, title: showdata.title})
+})
+// handler #3
 app.all ('*', (request, response) => {
     return response.sendStatus(404)
 })
-
+// directions on where to send data in browser
 app.listen (1337, () =>{console.log('listening on port 1337...') //eslint-disable-line no-comsole
 })
